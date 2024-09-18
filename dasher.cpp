f1855ac58d58fda8bd83392f567c8f4f8c9e5721
terafora <charlie.stone649@gmail.com>
2024-09-18 13:12:41 +0100

    Implementing velocity and "jumping"

--- dasher.cpp
+++ dasher.cpp
@@ -3,17 +3,35 @@
 int main() {
 
     // Window dimensions
-    const int windowWidth = 512;
-    const int windowHeight = 380;
+    const int windowWidth{512};
+    const int windowHeight{380};
 
     // Initialize window
     InitWindow(windowWidth, windowHeight, "Pixel Dasher");
+
+    // Rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity {0};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()) {
         // Begin drawing
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        // Check for input
+        if (IsKeyDown(KEY_SPACE)) {
+            velocity = -5;
+        } else {
+            velocity = 5;
+        }
+
+        posY += velocity;
+
+        DrawRectangle(windowWidth / 2, posY, width, height, MAROON);
 
         // Stop drawing
         EndDrawing();
